919400c7124a83d61088b838352deb08c4c8624e
julia franzoi <julia_franzoi@hotmail.com>
2017-02-24 11:01:38 +0000

    add player and pokemon

---
+++ client/src/models/player.js
@@ -0,0 +1,9 @@
+var Player = function(options){
+  this.name = options.name;
+  this.pokedex = options.pokedex || [];//unlimited
+  this.pokemon_on_hand = options.pokemon_on_hand//4x
+  this.pic = this.pic//url
+  this.x = this.x//600
+  this.y = this.y//600
+
+}
---
+++ client/src/models/pokemon.js
@@ -0,0 +1,7 @@
+var Pokemon = function(options){
+  this.name = options.name;
+  this.attack = options.attack;
+  this.defense = options.defense;
+  this.hp = options.hp;
+  this.picture = options.picture;
+}
