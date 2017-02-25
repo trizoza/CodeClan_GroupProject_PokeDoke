99e373069d78b90f6885a0e0d90ace6736ea1c57
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-02-25 00:33:45 +0000

    move ash around, inside canvas

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -71,9 +71,11 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 var UI = __webpack_require__(1);
+var Map = __webpack_require__(2);
 
 var app = function() {
-  new UI();
+  // new UI();
+  new Map();
 };
 
 window.onload = app;
@@ -82,72 +84,138 @@ window.onload = app;
 /* 1 */
 /***/ (function(module, exports) {
 
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
 //// HANDLE MOVEMENT ON MAP ////////////
 
 
 //// need to require player for coordinates ////////
-var UI = function() {
-
+var Map = function() {
   var canvas = document.querySelector("#map");
   var context = canvas.getContext('2d');
+  var x = 300;
+  var y = 200;
+  var increment = 20;
+
   var ash = document.createElement('img');
   ash.src = "/img/ash.png";
   var ashWidth = 40;
   var ashHeight = ashWidth;
-  var x = 150;
-  var y = 150;
-  var increment = 20;
+  var house = document.createElement('img');
+  house.src = "/img/house.png";
+  var gym = document.createElement('img');
+  gym.src = "/img/gym.png";
+  var grass = document.createElement('img');
+  grass.src = "/img/grass.png";
+  var pavement = document.createElement('img');
+  pavement.src = "/img/pavement.jpeg";
 
+  loadCanvas = function() {
+    
+    pavement.onload = function() {
+      context.drawImage(this, 0, 0, 600, 400);
 
-  var drawAsh = function(image) {
-    if (!image) image = this;
-    context.drawImage(ash, x - 20, y - 20, ashWidth, ashHeight);
-    console.log('draw ash', ash.src);
+      ash.onload = function() {
+        context.drawImage(this, x - ashWidth/2, y - ashWidth/2, ashWidth, ashHeight);
+      };
+
+      house.onload = function() {
+        context.drawImage(this, 40, 240, 120, 120);
+      };
+
+      gym.onload = function() {
+        context.drawImage(this, 40, 0, 220, 160);
+        context.drawImage(this, 340, 0, 220, 160);
+      };
+      
+      grass.onload = function() {
+        context.drawImage(this, 240, 220, 160, 260);
+        context.drawImage(this, 360, 220, 160, 260);
+        context.drawImage(this, 490, 220, 160, 260);
+      };
+
+      drawMap();
+      context.drawImage(ash, x - ashWidth/2, y - ashWidth/2, ashWidth, ashHeight);
+    };
+
   };
 
-  var moveAsh = function(image, xInc, yInc) {
-    if (!image) image = this;
-    context.drawImage(ash, x - 20 + xInc, y - 20 + yInc, ashWidth, ashHeight);
-    console.log('draw ash', ash.src);
+
+  drawMap = function() {
+    context.drawImage(pavement, 0, 0, 600, 400);
+    context.drawImage(house, 40, 240, 120, 120);
+    context.drawImage(gym, 40, 0, 220, 160);
+    context.drawImage(gym, 340, 0, 220, 160);
+    context.drawImage(grass, 240, 220, 160, 260);
+    context.drawImage(grass, 360, 220, 160, 260);
+    context.drawImage(grass, 490, 220, 160, 260);
   };
 
-  if (ash.complete) {
-    drawAsh(ash);
-  }
-  else {
-    ash.onload = drawAsh;
-  }
+
+  var moveAsh = function(xInc, yInc) {
+    drawMap();
+    context.drawImage(ash, x - ashWidth/2 + xInc, y - ashWidth/2 + yInc, ashWidth, ashHeight);
+    x += xInc;
+    y += yInc;
+    console.log(x,y);
+  };
+
+  ////here vvv
+
 
   document.onkeydown = function(event) {
     console.log(event.keyCode);
 
     if (event.keyCode === 39) {
       // right
-      if (ash.complete) {
-        moveAsh(ash, increment, 0);
+      if (x >= 600 - increment) {
+        moveAsh(0, 0);
       }
-      else {
-        ash.onload = moveAsh;
+      else{
+        moveAsh(increment, 0);
       }
-      x += increment;
     }
 
     if (event.keyCode === 37) {
       // left
+      if (x <= 0 + increment) {
+        moveAsh(0, 0);
+      }
+      else{
+        moveAsh(-increment, 0);
+      }
     }
 
     if (event.keyCode === 38) {
       // up
+      if (y <= 0 + increment) {
+        moveAsh(0, 0);
+      }
+      else {
+        moveAsh(0, -increment);
+      }
     }
 
     if (event.keyCode === 40) {
       // down
+      if (y >= 400 - increment) {
+        moveAsh(0, 0);
+      }
+      else {
+        moveAsh(0, increment);
+      }
     }
   };
 
+  loadCanvas();
+
 };
 
-module.exports = UI;
+module.exports = Map;
 
 /***/ })
 /******/ ]);
--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,9 @@
 var UI = require('./views/ui');
+var Map = require('./views/map');
 
 var app = function() {
-  new UI();
+  // new UI();
+  new Map();
 };
 
 window.onload = app;
