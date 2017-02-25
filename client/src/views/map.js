ec40aa65b40eb20a74ba5cf330af58766859d0ee
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-02-25 15:24:42 +0000

    fix resolution issues

--- client/src/views/map.js
+++ client/src/views/map.js
@@ -4,13 +4,13 @@
 var Map = function() {
   var canvas = document.querySelector("#map");
   var context = canvas.getContext('2d');
-  var x = 115;
-  var y = 80;
-  var increment = 5;
+  var x = 300;
+  var y = 200;
+  var increment = 10;
 
   var ash = document.createElement('img');
   ash.src = "/img/ash.png";
-  var ashWidth = 20;
+  var ashWidth = 40;
   var ashHeight = ashWidth;
   var house = document.createElement('img');
   house.src = "/img/house.png";
@@ -24,44 +24,60 @@ var Map = function() {
   loadCanvas = function() {
     
     pavement.onload = function() {
-      context.drawImage(this, 0, 0, 230, 190);
-
+      context.drawImage(this, 0, 0, 580, 460);
       ash.onload = function() {
-        context.drawImage(this, x - ashWidth/2, y - ashWidth/2, ashWidth, ashHeight);
+        context.drawImage(this, x - 20, y - 20, ashWidth, ashHeight);
       };
-
       house.onload = function() {
-        context.drawImage(this, 0, 0);
+        context.drawImage(this, 0, 270, 150, 130);
       };
-
       gym.onload = function() {
-        context.drawImage(this, 20, 0);
-        context.drawImage(this, 120, 0);
+        context.drawImage(this, 0, 0, 220, 170);
+        context.drawImage(this, 320, 0, 220, 170);
       };
-      
       grass.onload = function() {
-        context.drawImage(this, 200, 100);
+        context.drawImage(this, 540, 400, 40, 60);
       };
-
       drawMap();
-      context.drawImage(ash, x - ashWidth/2, y - ashWidth/2, ashWidth, ashHeight);
-    };
-
+      
+    }; 
+    context.drawImage(ash, x - 20 + xInc, y - 20 + yInc, ashWidth, ashHeight);
   };
 
-////// after the images are loaded for use, draw them in proper order
   drawMap = function() {
-    context.drawImage(pavement, 0, 0, 230, 190);
-    context.drawImage(house, 0, 110, 70, 60);
-    context.drawImage(gym, 20, 0, 100, 80);
-    context.drawImage(gym, 120, 0, 80, 40);
-    context.drawImage(grass, 200, 100, 60, 60);
+    context.drawImage(pavement, 0, 0, 580, 460);
+    context.drawImage(house, 0, 270, 150, 130);
+    context.drawImage(gym, 0, 0, 220, 170);
+    context.drawImage(gym, 360, 0, 220, 170);
+    context.drawImage(grass, 540, 400, 40, 60);
+    context.drawImage(grass, 500, 400, 40, 60);
+    context.drawImage(grass, 460, 400, 40, 60);
+    context.drawImage(grass, 420, 400, 40, 60);
+    context.drawImage(grass, 380, 400, 40, 60);
+    context.drawImage(grass, 340, 400, 40, 60);
+    context.drawImage(grass, 300, 400, 40, 60);
+    context.drawImage(grass, 260, 400, 40, 60);
+    context.drawImage(grass, 540, 340, 40, 60);
+    context.drawImage(grass, 500, 340, 40, 60);
+    context.drawImage(grass, 460, 340, 40, 60);
+    context.drawImage(grass, 420, 340, 40, 60);
+    context.drawImage(grass, 380, 340, 40, 60);
+    context.drawImage(grass, 340, 340, 40, 60);
+    context.drawImage(grass, 300, 340, 40, 60);
+    context.drawImage(grass, 260, 340, 40, 60);
+    context.drawImage(grass, 540, 280, 40, 60);
+    context.drawImage(grass, 500, 280, 40, 60);
+    context.drawImage(grass, 460, 280, 40, 60);
+    context.drawImage(grass, 420, 280, 40, 60);
+    context.drawImage(grass, 380, 280, 40, 60);
+    context.drawImage(grass, 340, 280, 40, 60);
+    context.drawImage(grass, 300, 280, 40, 60);
+    context.drawImage(grass, 260, 280, 40, 60);
   };
-
 
   var moveAsh = function(xInc, yInc) {
     drawMap();
-    context.drawImage(ash, x - ashWidth/2 + xInc, y - ashWidth/2 + yInc, ashWidth, ashHeight);
+    context.drawImage(ash, x - 20 + xInc, y - 20 + yInc, ashWidth, ashHeight);
     x += xInc;
     y += yInc;
     console.log(x,y);
@@ -69,19 +85,12 @@ var Map = function() {
 
   document.onkeydown = function(event) {
     console.log(event.keyCode);
-
     if (event.keyCode === 39) {
       // right
-      if (x >= 600 - increment) {
+      if (x >= 560) {
         moveAsh(0, 0);
       }
-      else if (x === 20 && 240 <= y && y <= 360) {
-        moveAsh(0, 0);
-      }
-      else if (x === 20 && 20 <= y && y <= 160) {
-        moveAsh(0, 0);
-      }
-      else if (x === 320 && 20 <= y && y <= 160) {
+      else if (x === 340 && 20 <= y && y <= 180) {
         moveAsh(0, 0);
       }
       else{
@@ -91,16 +100,13 @@ var Map = function() {
 
     if (event.keyCode === 37) {
       // left
-      if (x <= 0 + increment) {
+      if (x <= 20) {
         moveAsh(0, 0);
       }
-      else if (x === 180 && 240 <= y && y <= 360) {
+      else if (x === 240 && 20 <= y && y <= 180) {
         moveAsh(0, 0);
       }
-      else if (x === 280 && 20 <= y && y <= 160) {
-        moveAsh(0, 0);
-      }
-      else if (x === 580 && 20 <= y && y <= 160) {
+      else if (x === 170 && 260 <= y && y <= 410) {
         moveAsh(0, 0);
       }
       else{
@@ -110,20 +116,18 @@ var Map = function() {
 
     if (event.keyCode === 38) {
       // up
-      if (y <= 0 + increment) {
+      if (y <= 20) {
         moveAsh(0, 0);
       }
-      else if (y === 380 && 40 <= x && x <= 160) {
+      else if (y === 420 && 20 <= x && x <= 160) {
         moveAsh(0, 0);
       }
-      else if (y === 180 && 40 <= x && x <= 260) {
+      else if (y === 190 && 20 <= x && x <= 230) {
         moveAsh(0, 0);
       }
-      else if (y === 180 && 340 <= x && x <= 560) {
+      else if (y === 190 && 350 <= x && x <= 560) {
         moveAsh(0, 0);
       }
-  
-
       else {
         moveAsh(0, -increment);
       }
@@ -131,10 +135,10 @@ var Map = function() {
 
     if (event.keyCode === 40) {
       // down
-      if (y >= 400 - increment) {
+      if (y >= 440) {
         moveAsh(0, 0);
       }
-      else if (y === 220 && 40 <= x && x <= 160) {
+      else if (y === 250 && 20 <= x && x <= 160) {
         moveAsh(0, 0);
       }
       else {
