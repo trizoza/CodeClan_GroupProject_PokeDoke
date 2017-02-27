34ce54b31bb8b9f475cfe1d544ecb548211ea6a7
Rory MacDonald <rorymacdonald6@hotmail.com>
2017-02-27 17:52:26 +0000

    master without bundles

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -63,15 +63,17 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 5);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 5:
 /***/ (function(module, exports) {
 
 
 
 /***/ })
-/******/ ]);
+
+/******/ });
 //# sourceMappingURL=bundle.js.map
